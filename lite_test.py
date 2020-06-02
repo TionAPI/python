fedef47792925d7b9efa5991a9a3556fd8eac95d
IATkachenko <IATkachenko@gmail.com>
2020-06-02 17:13:01 +0300

    Tion lite decoder

---
+++ lite_test.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+from tion.lite import Lite
+
+_LOGGER = logging.getLogger(__name__)
+
+try:
+    mac = sys.argv[1]
+except IndexError:
+    mac = "dummy"
+
+device = Lite(mac)
+
+device.get()
+print("crc is: " + bytes(device._crc).hex())
+
+print("header._package_size = %s" % device._package_size)
+print("header_commad_type = %s" % bytes(device._command_type).hex())
+
+
